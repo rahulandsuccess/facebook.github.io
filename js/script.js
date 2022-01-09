a0dc13e856ba38e47aedcfb1f571c53d849c50ae
rahulandsuccess <92322022+rahulandsuccess@users.noreply.github.com>
2022-01-08 19:47:32 -0800

    facebook website

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function profileSettings(){
+    console.log('fuck')
+    let info = document.getElementById('all-info');
+    if(info.style.display == 'block'){
+        info.style.display = 'none'
+    }
+    else{
+        info.style.display = 'block';
+    }
+}
